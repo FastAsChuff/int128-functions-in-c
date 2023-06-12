dcb36e93ed0e9bc20692625aae849c9600540471
FastAsChuff <124365713+FastAsChuff@users.noreply.github.com>
2023-06-12 12:28:53 +0100

    Add files via upload

---
+++ modinv.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include </home/simon/int128fns.c>
+
+int main(int32_t argc, char* argv[]) {
+  myint128_t A, C, N;
+  A.i128 = 1;
+  N.i128 = 1;
+  if (argc > 1) strtomyint128((unsigned char*)argv[1], &A);
+  if (argc > 2) strtomyint128((unsigned char*)argv[2], &N);
+  modinv(A, N, &C);
+  print_myint128(C);
+  printf("\n");
+  printint128hex(C.i128);
+  printf("\n");
+  exit(0);
+}
