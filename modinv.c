53f2c82815953ec5c027edbd088198b2701f67c6
FastAsChuff <124365713+FastAsChuff@users.noreply.github.com>
2023-06-12 16:34:05 +0100

    Update modinv.c
    Added message for no inverse.

--- modinv.c
+++ modinv.c
@@ -8,15 +8,20 @@
 #include </home/simon/int128fns.c>
 
 int main(int32_t argc, char* argv[]) {
+  int32_t res;
   myint128_t A, C, N;
   A.i128 = 1;
   N.i128 = 1;
   if (argc > 1) strtomyint128((unsigned char*)argv[1], &A);
   if (argc > 2) strtomyint128((unsigned char*)argv[2], &N);
-  modinv(A, N, &C);
-  print_myint128(C);
-  printf("\n");
-  printint128hex(C.i128);
-  printf("\n");
+  res = modinv(A, N, &C);
+  if (res == 0) {
+    print_myint128(C);
+    printf("\n");
+    printint128hex(C.i128);
+    printf("\n");
+  } else {
+    printf("No inverse\n");
+  }
   exit(0);
 }
